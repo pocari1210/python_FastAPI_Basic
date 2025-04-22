6e433d4e43f9fcee1a1e5c93b9ab654b23669056
pocari1210 <pocari.design@gmail.com>
2025-04-22 10:16:51 +0900

    7-その他のルート設定

--- routers/contact.py
+++ routers/contact.py
@@ -34,16 +34,16 @@ async def create_contact(body: contact_schema.Contact):
     return contact_schema.Contact(**body.model_dump())
 
 # 詳細表示
-@router.get("/contacts/{id}")
-async def get_contact():
-    pass
+@router.get("/contacts/{id}", response_model=contact_schema.Contact)
+async def get_contact(id: int):
+    return contact_schema.Contact(id)
 
 # 更新
-@router.put("/contacts/{id}")
-async def update_contact():
-    pass
+@router.put("/contacts/{id}", response_model=contact_schema.Contact)
+async def update_contact(id: int, body: contact_schema.Contact):
+    return contact_schema.Contact(id, **body.model_dump())
 
 # 削除
-@router.delete("/contacts/{id}")
-async def delete_contact():
-    pass
+@router.delete("/contacts/{id}", response_model=contact_schema.Contact)
+async def delete_contact(id: int):
+    return
